c5536c41e90068e2e5626545d894ffc352ff157f
CIPHER <vudinhtoan0338868178@gmail.com>
2025-10-14 09:14:04 +0700

    ReadingFilePDF

--- ChatBotAI/Program.cs
+++ ChatBotAI/Program.cs
@@ -1,8 +1,10 @@
+using ChatApp1.Services;
+using ChatApp1.Services.Ingestion;
+using ChatBotAI.Application;
 using ChatBotAI.Components;
+using ChatBotAI.Infrastructure;
 using Microsoft.Extensions.AI;
 using OllamaSharp;
-using ChatBotAI.Application;
-using ChatBotAI.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,9 +14,19 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 IChatClient chatClient = new OllamaApiClient(new Uri("http://localhost:11434"),"llama3.2");
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = new OllamaApiClient(new Uri("http://localhost:11434"),
+    "all-minilm");
+
+var vectorStorePath = Path.Combine(AppContext.BaseDirectory, "vector-store.db");
+var vectorStoreConnectionString = $"Data Source={vectorStorePath}";
+builder.Services.AddSqliteCollection<string, IngestedChunk>("data-chatapp1-chunks", vectorStoreConnectionString);
+builder.Services.AddSqliteCollection<string, IngestedDocument>("data-chatapp1-documents", vectorStoreConnectionString);
+
+builder.Services.AddScoped<DataIngestor>();
+builder.Services.AddSingleton<SemanticSearch>();
 
 builder.Services.AddChatClient(chatClient).UseFunctionInvocation().UseLogging();
-
+builder.Services.AddEmbeddingGenerator(embeddingGenerator);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,12 +38,11 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+await DataIngestor.IngestDataAsync(
+    app.Services,
+    new PDFDirectorySource(Path.Combine(builder.Environment.WebRootPath, "Data")));
 app.Run();
